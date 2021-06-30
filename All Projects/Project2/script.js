03afb6f02f025ebc96e78afaa9da96b6704b4435
Shajii <86205958+Shajii@users.noreply.github.com>
2021-06-30 08:28:25 +0500

    Project 2 css completed.

---
+++ All Projects/Project2/script.js
@@ -0,0 +1,6 @@
+// Get DOM Elements
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.seats');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const selectMovie = document.getElementById('movie');

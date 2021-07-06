a120cab61d6e7be14a6684833c2ddc230c8f0eb0
Shajii <86205958+Shajii@users.noreply.github.com>
2021-07-06 05:05:23 +0500

    Final update of three projects

--- All Projects/Project3/script.js
+++ All Projects/Project3/script.js
@@ -7,27 +7,45 @@ const time = document.getElementById('time');
 
 // Function to play or pause video
 function playPause() {
-
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
 };
 
 // Function to play or pause icons
 function updateIcons() {
-
+        if (video.paused) {
+             play.innerHTML = '<i class="fa fa-play fa-2x"></i>'; 
+        } else {
+            play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+        }
 };
 
 // Function to update the video progress
 function updateProgress() {
-
+        progress.value = (video.currentTime/video.duration)*100; 
+        let minutes = Math.floor(video.currentTime/60);
+        if (minutes < 10) {
+                minutes = '0'+ String(minutes);
+        }
+        let seconds = Math.floor(video.currentTime%60);
+        if (seconds < 10) {
+                seconds = '0'+ String(seconds);
+        }
+        time.innerHTML = minutes+':'+seconds;
 };
 
 // Function to stop video
 function stopVideo() {
-
+        video.currentTime = 0;
+        video.pause();
 };
 
 // Function to update video progress
 function updateVideoProgress() {
-
+       video.currentTime = (progress.value*video.duration)/100;
 };
 
 // Event Listener
